6ee8ac98f83ba55cc65eee8fea7fc3d244a45d72
qiuchenwenbin <qiuchenwenbin@126.com>
2025-12-08 11:39:47 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'bingo-quiz-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './xlsx.full.min.js',
+    // 'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js' 
+    './icon.png' // 如果你有图标的话
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
